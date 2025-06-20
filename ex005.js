6f8e4b9c06a7aa3b7a25cfa6d2a46ae124cd21b3
Matheus Lins <matheuslins134@gmail.com>
2025-06-20 19:22:45 -0300

    att
    adicionado novo aprendizado:
    -if e else (condições)

---
+++ ex005.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`você ultrapassou a velocidade perfeita. MULTADO!`)
+
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
