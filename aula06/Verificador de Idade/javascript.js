c625031b362eea33e8d5c44ddded49ed0ef01624
Matheus Lins <matheuslins134@gmail.com>
2025-07-31 18:47:51 -0300

    att
    adição de novo projeto

---
+++ aula06/Verificador de Idade/javascript.js
@@ -0,0 +1,77 @@
+function verificar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+
+    } else {
+
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+
+            gênero = 'Homem'
+            if (idade >=0 && idade < 12){
+
+                // Criança
+                img.setAttribute('src', 'masc-criança.jpg')
+
+            } else if (idade < 21){
+
+                // Jovem
+                img.setAttribute('src', 'masc-jovem.jpg')
+
+            } else if (idade < 50) {
+
+                // Adulto
+                img.setAttribute('src', 'masc-adulto.jpg')
+
+            } else {
+
+                // idoso
+                img.setAttribute('src', 'masc-velho.jpg')
+            }
+
+
+        } else if (fsex[1].checked) {
+
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 12){
+
+                // Criança
+                img.setAttribute('src', 'fem-criança.jpg')
+
+            } else if (idade < 21) {
+
+                // Jovem
+                img.setAttribute('src', 'fem-jovem.jpg')
+
+            } else if (idade < 50) {
+
+                // Adulta
+                img.setAttribute('src', 'fem-adulta.jpg')
+
+            } else {
+
+                // Idoso
+                img.setAttribute('src', 'fem-velha.jpg')
+
+            }
+
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
